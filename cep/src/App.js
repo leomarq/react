6a3994297991101a94454f3325388334c42f4e49
leomarq <leoglmarques2@gmail.com>
2023-02-28 23:02:29 -0300

    full project skeleton

--- cep/src/App.js
+++ cep/src/App.js
@@ -1,7 +1,23 @@
+import {useState} from "react";
+import { FiSearch } from "react-icons/fi";
+import apiCep from './services/apiCep.js';
+
 function App() {
+  const [inputCepDigitado,setinputCepDigitado] = useState('');
+  const [cep,setCep] = useState('');
+    async function pesquisarCep() {
+      //alert ('valor digitado: ' + inputCepDigitado)
+      const responseCep = await apiCep.get(`${inputCepDigitado}/json`)
+      console.log(responseCep.data);
+    }
   return (
     <div>
       <h1>BUSCADOR DE CEP</h1>
+      <p>CEP: <input type="text" placeholder="Digite seu CEP" value={inputCepDigitado} onChange={(event) => setinputCepDigitado(event.target.value)}></input>
+      <button onClick={pesquisarCep}>
+        <FiSearch/>
+      </button>
+      </p>
     </div>
   );
 }
